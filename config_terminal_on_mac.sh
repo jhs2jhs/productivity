cd93d577bf969ed74b6f8ce43509668eed152d61
Jianhua Shao <jianhua.shao1986@gmail.com>
2018-05-13 17:52:55 +0100

    install powerlevel9

--- config_terminal_on_mac.sh
+++ config_terminal_on_mac.sh
@@ -1,23 +1,38 @@
+##########################################################################
 # install term2
+##########################################################################
 ## http://www.iterm2.com/downloads.html
 brew cask install term2
 
+
+##########################################################################
 # install term2 theme:
+##########################################################################
 ## https://github.com/mbadolato/iTerm2-Color-Schemes
 
+
+##########################################################################
 # install tree
+##########################################################################
 brew install tree
 
+##########################################################################
 # install ack: search keywords in the files. 
+##########################################################################
 brew install ack
 # ack --js pancakes
 
+##########################################################################
 # install autoenv
+##########################################################################
 ## https://github.com/kennethreitz/autoenv
 brew install autoenv
 echo "source $(brew --prefix autoenv)/activate.sh" >> ~/.bash_profile
 
+
+##########################################################################
 # install oh-my-zsh
+##########################################################################
 ## https://github.com/robbyrussell/oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
@@ -25,6 +40,68 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 ## https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins
 # vi ~/.zshrc
 # modify : plugins
+
+##########################################################################
+# hide "user@hostname"
+##########################################################################
+## add DEFAULT_USER=$USER in the end of .zshrc
+echo "DEFAULT_USER=$USER" >> .zshrc
+
+##########################################################################
+# install zsh syntax highlighting
+##########################################################################
+## https://gist.github.com/kevin-smets/8568070
+brew install zsh-syntax-highlighting
+# add bellow line into .zshrc
+echo "source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+
+##########################################################################
+# install zsh-autosuggestions.zsh
+##########################################################################
+## https://github.com/zsh-users/zsh-autosuggestions
+brew install zsh-autosuggestions
+echo "source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+
+##########################################################################
+# install zsh-completions
+##########################################################################
+## https://github.com/zsh-users/zsh-completions
+brew install zsh-completions
+echo "fpath=(/usr/local/share/zsh-completions $fpath)" >> ~/.zshrc
+rm -f ~/.zcompdump; compinit
+chmod go-w '/usr/local/share'
+# git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
+## Enable it in your .zshrc by adding it to your plugin list and reloading the completion:
+## plugins=(… zsh-completions)
+## autoload -U compinit && compinit
+
+##########################################################################
+# install nerd font
+##########################################################################
+brew tap caskroom/fonts 
+brew cask install font-hack-nerd-font
+echo 'POWERLEVEL9K_MODE="nerdfont-complete"' >> ~/.zshrc
+# https://al03.github.io/%E8%AE%A9%E5%91%BD%E4%BB%A4%E8%A1%8C%E6%9B%B4%E7%82%AB%E9%85%B7/
+# change font (both ascii and non-ascii) in iterm2 into "Knack Nerd Font"
+
+
+##########################################################################
+# install powerlevel9k theme
+##########################################################################
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+## modify ~/.zshrc: 
+echo 'ZSH_THEME="powerlevel9k/powerlevel9k"' >> ~/.zshrc
+
+
+
+
+
+##########################################################################
+##########################################################################
+##########################################################################
+##########################################################################
+# backup
+##########################################################################
 
 # install powerline fonts
 ## https://github.com/powerline/fonts
@@ -42,9 +119,6 @@ echo "\ue0b0 \u00b1 \ue0a0 \u27a6 \u2718 \u26a1 \u2699"
 # hide "user@hostname"
 ## add DEFAULT_USER=$USER in the end of .zshrc
 
-# install powerlevel9k theme
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-## modify ~/.zshrc: ZSH_THEME="powerlevel9k/powerlevel9k"
 
 # install awesome-terminal
 ## https://github.com/gabrielelana/awesome-terminal-fonts
