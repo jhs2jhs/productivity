44e948601cda8146dfd42b12239b490eea603289
Jianhua Shao <jianhua.shao1986@gmail.com>
2018-05-13 14:38:19 +0100

    open terminal from here in finder

---
+++ config_finder_on_mac.sh
@@ -0,0 +1,6 @@
+# install termhere: https://hbang.ws/apps/termhere/
+
+# install go2shell: 
+## https://itunes.apple.com/gb/app/go2shell/id445770608?mt=12 
+## http://macappstore.org/go2shell/
+brew cask install go2shell
