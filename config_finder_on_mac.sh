0904aaf7505e7e1df83321ee7557fe5e03167b9a
Jianhua Shao <jianhua.shao1986@gmail.com>
2018-05-20 12:02:56 +0100

    Update config_finder_on_mac.sh

--- config_finder_on_mac.sh
+++ config_finder_on_mac.sh
@@ -8,3 +8,6 @@ brew install carthage
 ## https://itunes.apple.com/gb/app/go2shell/id445770608?mt=12 
 ## http://macappstore.org/go2shell/
 brew cask install go2shell
+
+# install cheathseet 
+https://www.mediaatelier.com/CheatSheet/
