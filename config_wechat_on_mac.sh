edbf7d0acd59e3e3685508e7695becce1b6bea1c
Jianhua Shao <jianhua.shao1986@gmail.com>
2018-05-20 11:08:01 +0100

    Create config_wechat_on_mac.sh

---
+++ config_wechat_on_mac.sh
@@ -0,0 +1,3 @@
+# https://github.com/jianhuashao/WeChatPlugin-MacOS
+
+cd ~/Downloads && rm -rf WeChatPlugin-MacOS && git clone https://github.com/TKkk-iOSer/WeChatPlugin-MacOS.git --depth=1 && ./WeChatPlugin-MacOS/Other/Install.sh
