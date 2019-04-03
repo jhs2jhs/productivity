c237797520004397f6884fe761200598f2e52929
Jianhua Shao <jianhua.shao1986@gmail.com>
2019-04-03 04:44:53 +0100

    Update config_wechat_on_mac.sh

--- config_wechat_on_mac.sh
+++ config_wechat_on_mac.sh
@@ -1,4 +1,10 @@
 # https://github.com/jianhuashao/WeChatPlugin-MacOS
 
 # suport auto-auth
-cd ~/Downloads && rm -rf WeChatPlugin-MacOS && git clone https://github.com/TKkk-iOSer/WeChatPlugin-MacOS.git --depth=1 && ./WeChatPlugin-MacOS/Other/Install.sh
+cd ~/Downloads 
+rm -rf WeChatPlugin-MacOS 
+git clone https://github.com/TKkk-iOSer/WeChatPlugin-MacOS.git --depth=1 
+./WeChatPlugin-MacOS/Other/Install.sh
+
+
+
